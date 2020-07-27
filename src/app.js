a2fadca3f0d30503a7800e63fe784f84b7c575ab
peaky76 <robertjamespeacock@gmail.com>
2020-07-27 18:03:31 +0100

    list display and add function

--- src/app.js
+++ src/app.js
@@ -3,5 +3,22 @@ import Vue from "vue";
 document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
+    data: {
+      toDoList: [
+        { description: "Clean bathroom", priority: "low" },
+        { description: "Have a few beers", priority: "high" },
+        { description: "Change baby's nappy", priority: "low" },
+      ],
+      newItem: "",
+    },
+    methods: {
+      addNewItem: function () {
+        this.toDoList.push({
+          description: this.newItem,
+          priority: "low",
+        });
+        this.newItem = "";
+      },
+    },
   });
 });
